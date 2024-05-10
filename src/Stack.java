f2acc9b81a194f933169ff5a440e3d214aeb6840
Aqsaazizwagan <aqsawagan8@gmail.com>
2024-05-10 10:29:23 +0500

    printed name

--- src/Stack.java
+++ src/Stack.java
@@ -1,5 +1,7 @@
 public class Stack {
     public static void main(String[] args) {
         System.out.println("Hello world java");
+        System.out.println("Iam aqsa aziz");
+        System.out.println("Iam aqsa aziz 2");
     }
 }
